f88694f381096c7d204c785f38714a1719e69660
Tim BOWMAN <puffy@netherlogic.com>
2010-08-31 14:56:28 -0400

    Remove test_output.py

--- test_output.py
+++
@@ -1,9 +0,0 @@
-
-
-cmd = "nuke -ix testScene.nk"
-
-cmd = " ".join([nuke.env['ExecutablePath'], flags, '-F', '"' + instRange + '"', nuke.value("root.name"), '&>', logFile])
-print ">>> starting instance %d" % (i, )
-print "command: " + cmd
-subprocess.Popen(cmd, shell=True)
-    
