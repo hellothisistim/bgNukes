3eccb66fb1d63f13d9052852b4cc23fa858e2642
Tim BOWMAN <puffy@netherlogic.com>
2014-03-12 10:10:29 -0400

    Switch to shell=True
    This doesn't fail in Nuke8's Python 2.7. Still testing to see if it's a
    proper solution to Issue #9 (Broken in Nuke8.)

--- bgNukes.py
+++ bgNukes.py
@@ -94,7 +94,7 @@ def launch_nukes(nodes=[]):
         cmd = " ".join([nuke.env['ExecutablePath'], flags, '-F', '"' + instRange + '"', nuke.value("root.name"), '&>', logFile])
         print ">>> starting instance %d" % (i, )
         print "command: " + cmd
-        subprocess.Popen(cmd, shell=False)
+        subprocess.Popen(cmd, shell=True)
     
     nuke.message(str(inst) + ' renderers launched in the background.\nLog files: ' + ', '.join(logs))
     
